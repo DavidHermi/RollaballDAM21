4309bef71a186fe3bcc5b4bdbe1c3e9619481c3a
dam2a <dhermidaperez@danielcastelao.org>
2023-03-20 10:55:04 +0100

    Haciendo enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
-
+    private int count ;
     private float movementX;
     private float movementY;
 
@@ -16,6 +18,13 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText ();
+
+     
+        winTextObject.SetActive(false);
+        
     }
 
     private void OnMove(InputValue movementValue)
@@ -33,11 +42,29 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) 
     {
-        if (other.gameObject.CompareTag("PickUp"))
+       
+        if (other.gameObject.CompareTag ("PickUp"))
         {
-            other.gameObject.SetActive(false);
+            other.gameObject.SetActive (false);
+
+         
+            count = count + 1;
+
+            SetCountText ();
+        }
+    }
+    
+    
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12) 
+        {
+            
+            winTextObject.SetActive(true);
         }
     }
 }
