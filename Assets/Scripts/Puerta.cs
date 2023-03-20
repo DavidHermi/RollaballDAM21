e662a349a98aba7c1235d00bb12333e2f659cfeb
dhermidaperez@danielcastelao.org <dhermidaperez@danielcastelao.org>
2023-03-20 17:32:41 +0100

    Puerta giratoria acabada

---
+++ Assets/Scripts/Puerta.cs
@@ -0,0 +1,22 @@
+﻿namespace DefaultNamespace;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour {
+    public Animator laPuerta ;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        lapuerta.play("abrir");
+    }
+    private void OnTriggerexit(Collider other)
+    {
+
+        lapuerta.play("salir");
+    }
+   
+}
+
